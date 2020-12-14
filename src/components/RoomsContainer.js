bc607c0d71d981b46c18ecd7e4501750e23ac072
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-14 18:48:44 +0330

    RoomsList component completed

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -10,39 +10,12 @@ function RoomsContainer({ context }) {
     return <Loading></Loading>;
   }
   return (
-    <div>
-      rooms container
+    <>
       <RoomsFilter rooms={rooms}></RoomsFilter>
       <RoomsList rooms={sortedRooms}></RoomsList>
-    </div>
+    </>
   );
 }
 
 export default withRoomConsumer(RoomsContainer);
 
-// import React from "react";
-// import RoomsFilter from "./RoomsFilter";
-// import RoomsList from "./RoomsList";
-// import { Consumer } from "../context";
-// import Loading from "./Loading";
-// export default function RoomsContainer() {
-//   return (
-//     <>
-//       <Consumer>
-//         {(value) => {
-//           const { loading, sortedRooms, rooms } = value;
-//           if (loading) {
-//             return <Loading></Loading>;
-//           }
-//           return (
-//             <div>
-//               rooms container
-//               <RoomsFilter rooms={rooms}></RoomsFilter>
-//               <RoomsList rooms={sortedRooms}></RoomsList>
-//             </div>
-//           );
-//         }}
-//       </Consumer>
-//     </>
-//   );
-// }
--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Room from "./Room";
 
-export default function RoomsList() {
+export default function RoomsList({ rooms }) {
+  if (rooms.length === 0) {
     return (
-        <div>
-        hello from rooms list    
-        </div>
-    )
+      <div className="empty-search">
+        Unfortunately no rooms matched your search parameters
+      </div>
+    );
+  }
+  return (
+    <section className="roomslist">
+      <div className="roomslist-center">
+        {rooms.map((item) => {
+          return <Room key={item.id} room={item}></Room>;
+        })}
+      </div>
+    </section>
+  );
 }
