79a7eb7ea223e5db190c1f379f31fd2658fded7b
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-14 18:59:25 +0330

    RoomsFilter setup

--- src/context.js
+++ src/context.js
@@ -10,6 +10,15 @@ export default class RoomProvider extends Component {
     sortedRooms: [],
     featuredRooms: [],
     loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
   };
 
   // getData
@@ -18,11 +27,16 @@ export default class RoomProvider extends Component {
     // this.getData()
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter((room) => room.featured === true);
+    let maxPrice = Math.max(...rooms.map((item) => item.price));
+    let maxSize = Math.max(...rooms.map((item) => item.size));
     this.setState({
       rooms,
       sortedRooms: rooms,
       featuredRooms,
       loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize,
     });
   }
 
@@ -38,28 +52,45 @@ export default class RoomProvider extends Component {
     return tempItems;
   }
 
-  getRoom = (slug) =>{
-    let tempRooms = [...this.state.rooms]
-    return tempRooms.find((room) => room.slug === slug)
+  getRoom = (slug) => {
+    let tempRooms = [...this.state.rooms];
+    return tempRooms.find((room) => room.slug === slug);
+  };
 
-  }
+  handleChange = (event) => {
+    const type = event.target.type;
+    const name = event.target.name;
+    const value = event.target.value;
+    console.log(type, name, value);
+  };
+
+  filterRooms = () => {
+    console.log("filter rooms method");
+  };
 
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom : this.getRoom }}>
+      <RoomContext.Provider
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange,
+        }}
+      >
         {this.props.children}
       </RoomContext.Provider>
     );
   }
-
 }
 
-export function withRoomConsumer(Component){
-  return function ConsumerWrapper(props){
-   return <Consumer>
-      {value => <Component {...props} context={value}></Component>}
-    </Consumer>
-  }
+export function withRoomConsumer(Component) {
+  return function ConsumerWrapper(props) {
+    return (
+      <Consumer>
+        {(value) => <Component {...props} context={value}></Component>}
+      </Consumer>
+    );
+  };
 }
 
 export { RoomProvider, RoomContext, Consumer };
